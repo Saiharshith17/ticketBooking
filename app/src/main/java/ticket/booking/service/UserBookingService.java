818cf73088e1c5308f443678750c31ad7cca6aab
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-06 22:52:31 +0530

    added setters and getters

--- app/src/main/java/ticket/booking/service/UserBookingService.java
+++ app/src/main/java/ticket/booking/service/UserBookingService.java
@@ -7,6 +7,7 @@ import ticket.booking.entities.User;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
     private User user;
@@ -20,5 +21,11 @@ public class UserBookingService {
         this.user=user1;
         File users=new File(USERS_PATH);
         userList=objectMapper.readValue(users,new TypeReference<List<User>>(){});
+    }
+    public Boolean loginUser(){
+        Optional<User> foundUser=userList.stream().filter(user1->{
+            return user1.getName().equals(user.getName())&& UserServiceUtil.checkPassword(user.getPassword(),user)}).findFirst();
+        return foundUser.isPresent();
+        })
     }
 }
