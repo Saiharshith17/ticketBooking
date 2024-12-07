aa03ba3b52bd377770941affae05aa38edad8969
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-07 22:38:35 +0530

    added

--- app/src/main/java/ticket/booking/service/UserBookingService.java
+++ app/src/main/java/ticket/booking/service/UserBookingService.java
@@ -95,24 +95,20 @@ public class UserBookingService {
        return trainSelectedForBooking.getSeats();
     }
     public Boolean bookTrainSeat(Train train,Integer row,Integer seat){
-       try{
-           TrainService trainService=new TrainService();
-           List<List<Integer>> seats=train.getSeats();
-           if(row>=0&&row<seats.size()&&seat>=0&&seat<seats.get(row).size()){
-               if (seats.get(row).get(seat) == 0) {
-               seats.get(row).set(seat, 1);
-               train.setSeats(seats);
-               trainService.addTrain(train);
-               return true;
-           }
-           else{
-               return false;
-           }
-       } else{
-           return false;
+        TrainService trainService=new TrainService();
+        List<List<Integer>> seats=train.getSeats();
+        if(row>=0&&row<seats.size()&&seat>=0&&seat<seats.get(row).size()){
+            if (seats.get(row).get(seat) == 0) {
+            seats.get(row).set(seat, 1);
+            train.setSeats(seats);
+            trainService.addTrain(train);
+            return true;
         }
-       }catch (IOException ex){
-           return Boolean.FALSE;
-       }
+        else{
+            return false;
+        }
+    } else{
+        return false;
+     }
     }
 }
